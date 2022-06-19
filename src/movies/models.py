274d5da2882f51dfa3625cc4aad1cbfadb5dc311
kaetahr <lostech@tuta.io>
2022-06-18 19:53:26 -0500

    Revert "forces localhost host"
    This reverts commit e2fc03f2b57d01f426a33b807016de565caf841e.

--- src/movies/models.py
+++ src/movies/models.py
@@ -14,8 +14,7 @@ from sqlalchemy.ext.declarative import declarative_base
 dbname='localhost:5432/movies'
 
 def get_postgres_uri():
-    #host = os.environ.get("DB_HOST", "postgres")
-    host = 'localhost'
+    host = os.environ.get("DB_HOST", "postgres")
     port = 5432
     password = os.environ.get("DB_PASS", "abc123")
     user, db_name = "movies", "movies"
